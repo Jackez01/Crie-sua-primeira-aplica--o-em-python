3e2a0b30166cad4b197700849a9a9b8935d2a40d
Jackez01 <lucasgefreitas@gmail.com>
2026-04-01 21:45:25 -0300

    Finalizando exercicio gerenciador de tarefas.

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 tarefas = []
 
 def voltar_inicio():
@@ -13,34 +14,42 @@ def cadastrar_tarefa():
     voltar_inicio()
 
 def visualizar_tarefas():
-    print (f'Essas são suas tarefas:\n {tarefas}')
+    if tarefas:
+        for i, tarefa in enumerate(tarefas, 1):
+            print(f'{i}. {tarefa}')
+    else:
+        print('Nenhuma tarefa cadastrada.')
     voltar_inicio()
 
 def remover_tarefa():
     tarefa_escolhida = input('Digite a tarefa que você quer remover: ')
-    for tarefa in tarefas:
-        if tarefa_escolhida == tarefa:
-            tarefas.remove(tarefa_escolhida)
-        else:
-            print('Tarefa inválida!')
+    if tarefa_escolhida in tarefas:
+        tarefas.remove(tarefa_escolhida)
+        print('Tarefa removida com sucesso!')
+    else:
+        print('Tarefa inválida!')
 
-    print('Tarefa removida com sucesso!')
+    
     voltar_inicio()
 
 
-# def finalizar():
-
-
+def finalizar():
+    print('Finalizando o aplicativo')
+    sys.exit()
 
 
 def opcoes():
-    
-    escolha_usuario = int(input( 
-    '1.Adicionar tarefa\n'
-    '2. Visualizar tarefa\n' 
-    '3. Remover tarefa\n' 
-    '4. Sair\n'
-    'Escolha uma das opções: \n'))
+    try:
+        escolha_usuario = int(input( 
+        '1.Adicionar tarefa\n'
+        '2. Visualizar tarefa\n' 
+        '3. Remover tarefa\n' 
+        '4. Sair\n'
+        'Escolha uma das opções: \n'))
+    except ValueError:
+        print('Digite um número válido')
+        voltar_inicio()
+        return
     
     if escolha_usuario == 1:
         cadastrar_tarefa()
@@ -54,7 +63,7 @@ def opcoes():
 
     #limpa o terminal de acordo com o sistema operacional    
 def main():
-    if os.system=='nt':
+    if os.name=='nt':
         os.system('cls')
     else:
         os.system('clear')
